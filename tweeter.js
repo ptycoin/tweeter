7a981d5d24d4a3a9f78942a2b36285f2d01a6179
Jim Phillips <jim@panamabitcoins.com>
2014-08-14 21:23:31 +0100

    Added Facebook support

--- tweeter.js
+++ tweeter.js
@@ -1,4 +1,4 @@
-var twit = require('twit');
+var fb = require('fb');
 var bitfinex = require('bitfinex');
 var fs = require('fs');
 var request = require('request');
@@ -7,8 +7,23 @@ var path = require('path');
 // Read Configuration File
 var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'cfg.json')).toString());
 
+// Fetch Facebook Access Token
+fb.api('oauth/access_token', {
+    client_id: config.facebook_app_id,
+    client_secret: config.facebook_app_secret,
+    grant_type: 'client_credentials'
+  }, function (res) {
+    if(!res || res.error) {
+      console.log(!res ? 'error occurred' : res.error);
+      return;
+    }
+    config.facebook_access_token = res.access_token;
+    console.log(config.facebook_access_token);
+  });
+
 //var ticker = setInterval(tweetTicker, 15 * 60 * 1000);
-var balance = setInterval(tweetBalance, 15 * 60 * 1000);
+//var balance = setInterval(tweetBalance, 15 * 60 * 1000);
+bookBalance();
 
 // This function fetches the latest exchange rate and 
 // tweets an updated ticker
@@ -56,6 +71,42 @@ function tweetBalance() {
                     '#bitcoin #panama http://ptycoin.com/buy';
           var twitter = new twit(config);
           twitter.post('statuses/update', { status: msg }, function(err, data, response) { console.log(msg) });
+        } else {
+          console.log(err);
+        }
+      });
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function bookBalance() {
+  var bfx = new bitfinex(config.bitfinex_api, config.bitfinex_secret);
+  
+  // Fetch Balance
+  bfx.wallet_balances(function(err, res, body){
+    if (!err) {
+      var balance = parseFloat(res[3].available).toFixed(2);
+   
+      // Convert the balance to BTC at current ticker price
+      bfx.ticker('BTCUSD', function(err, res, body) {
+        if (!err) {
+          var ticker = parseFloat(res.ask).toFixed(2);
+          var btc = balance / ticker;
+
+          // Book the balance
+          var msg = 'PTYcoin currently has ' + btc.toFixed(4) + 
+                    ' BTC available at $' + ticker + ' + 1% fee. ' +
+                    '#bitcoin #panama http://ptycoin.com/buy';
+          fb.setAccessToken(config.facebook_access_token);
+          fb.api('panamabitcoins/feed', 'post', { message: msg }, function (res) {
+            if(!res || res.error) {
+              console.log(!res ? 'error occurred' : res.error);
+              return;
+            }
+            console.log('Post Id: ' + res.id);
+          });
         } else {
           console.log(err);
         }
